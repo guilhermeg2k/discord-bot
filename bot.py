86309cfaf41ec44f2334626ab5f5382766736d77
guilhermeg2k <guilhermen2k@gmail.com>
2021-09-13 23:31:36 -0300

    new: searching song by name

--- bot.py
+++ bot.py
@@ -1,9 +1,8 @@
 import os
 from discord.ext import commands
-from discord.player import FFmpegOpusAudio
 from discord import FFmpegPCMAudio
 from dotenv import load_dotenv
-import youtube_dl
+from youtube import download_song, get_song_url
 
 class Bot(commands.Bot):
     def __init__(self, command_prefix):
@@ -14,29 +13,26 @@ class Bot(commands.Bot):
         @self.event
         async def on_ready():
             print(f"{self.user.display_name} is connected")
-
+        
         @self.command()
-        async def join(ctx):
+        async def play(ctx, *, song):
             if ctx.author.voice is None:
                 await ctx.send("Você não está em um canal de voz")
                 return
             
             voice_channel = ctx.author.voice.channel
-
+            
             if ctx.voice_client is None:
                 await voice_channel.connect()
-            else:
-                await ctx.voice_client.move_to(voice_channel)
-        
-        @self.command()
-        async def play(ctx):
+ 
             voice_client = ctx.voice_client
-            voice_client.stop()
-            
-            song_path = self.download_song('songs', "https://www.youtube.com/watch?v=-tn2S3kJlyU")
-
-            voice_client.play(FFmpegPCMAudio(song_path))
-            print(song_path)
+            if voice_client.channel == voice_channel:
+                voice_client.stop()
+                song_url = get_song_url(song)
+                song_path = download_song('songs', song_url)
+                voice_client.play(FFmpegPCMAudio(song_path))
+            else:
+                await ctx.send("O bot já está conectado em outro canal!")
 
         @self.command()
         async def pause(ctx):
@@ -49,15 +45,3 @@ class Bot(commands.Bot):
             voice_client.resume()
 
         self.run(self.token)
-
-    def download_song(self, folder, url):
-        # Download the music
-        ydl_opts = {
-            'format': 'bestaudio/best',
-            'outtmpl': f'{folder}/%(id)s.%(ext)s',
-            }
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            song_info = ydl.extract_info(url)
-            file_name = f'{song_info["id"]}.webm'
-            ydl.download([url])
-        return f'{folder}/{file_name}'
