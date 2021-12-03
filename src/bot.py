acbec98d80ee8d8b0942120db291c516f02057c6
Henrique Mariano <henriquiui70@gmail.com>
2021-12-03 19:58:05 -0300

    + Adicionando a função __delete_message__ * Utilizada para apagar as mensagens de comando do usuário no canal.

--- src/bot.py
+++ src/bot.py
@@ -23,49 +23,64 @@ class Bot(commands.Bot):
         @self.command(aliases=['p'])
         async def play(ctx: commands.Context, *, play_text: str):
             self.logger.info('O bot recebeu uma solicitacao de play.')
+            await __delete_message__(ctx)
             await self.player.play(ctx, play_text)
 
         @self.command()
         async def pause(ctx: commands.Context):
+            await __delete_message__(ctx)
             await self.player.pause(ctx)
             self.logger.info('O bot pausou a música.')
 
         @self.command(aliases=['n', 's', 'skip'])
         async def next(ctx: commands.Context):
+            await __delete_message__(ctx)
             await self.player.next(ctx)
             self.logger.info('O bot pulou a música.')
 
         @self.command(aliases=['rs'])
         async def resume(ctx: commands.Context):
+            await __delete_message__(ctx)
             await self.player.resume(ctx)
             self.logger.info('O bot voltou a reproduzir a música.')
 
         @self.command(aliases=['l'])
         async def leave(ctx: commands.Context):
+            await __delete_message__(ctx)
             await self.player.leave(ctx)
+            self.logger.info('O bot saiu do canal de voz.')
 
         @self.command(aliases=['q', 'queue'])
         async def list(ctx: commands.Context):
+            await __delete_message__(ctx)
             await self.player.list(ctx)
+            self.logger.info('O bot listou a fila de reprodução.')
 
         @self.command(aliases=['r'])
         async def remove(ctx: commands.Context, *, idx: str):
+            await __delete_message__(ctx)
             await self.player.remove(ctx, idx)
         
         @self.command(aliases=['c', 'clean'])
         async def clear(ctx: commands.Context):
+            await __delete_message__(ctx)
             await self.player.clear(ctx)
 
         @self.command()
         async def shuffle(ctx: commands.Context):
+            await __delete_message__(ctx)
             await self.player.shuffle(ctx)
 
         @self.command(aliases=['ly'])
         async def lyrics(ctx: commands.Context, *, ly_text: str = None):
+            await __delete_message__(ctx)
             await self.player.lyrics(ctx, ly_text=ly_text)
-
+        
         @self.command()
         async def ping(ctx: commands.Context):
             await ctx.send("Pong!")
+
+        async def __delete_message__(ctx: commands.Context):
+            await ctx.message.delete()
 
         self.run(self.token)
